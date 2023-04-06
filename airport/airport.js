bbc75ecf02fe78ba6eae1388bf0d313afeefcda5
Parvin Sattorova <nanajon66@gmail.com>
2023-04-06 08:12:45 -0600

    test: add finished airport test

--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,42 @@
 
+function createAirport(name, airlines, availableGates){
+  var airport = {
+    name,
+    airlines,
+    availableGates,
+  };
+  return airport;
+};
+function welcomeGuests(airport){
+  return `Welcome to ${airport.name}!`
+};
+
+function landPlanes(airport, lended){
+  if(airport.availableGates > lended){
+    airport.availableGates = airport.availableGates - lended
+    return `Success! Current availability is ${airport.availableGates}.`;
+  }else{
+    var overflow = lended - airport.availableGates; 
+    airport.availableGates = 0;
+    return `Oh no! Not enough gates available. Current overflow is ${overflow}.`; 
+  } 
+};
+
+function checkAirlineLocations(airportList, airline) {
+  var connectedLocation = [];
+  for(var i=0; i<airportList.length; i++) {
+    if(airportList[i].airlines.includes(airline)) {
+      connectedLocation.push(airportList[i].name);
+    }
+  }
+  return connectedLocation;
+};
+
 
 
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
-  // checkAirlineLocations
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
+  checkAirlineLocations
 };
